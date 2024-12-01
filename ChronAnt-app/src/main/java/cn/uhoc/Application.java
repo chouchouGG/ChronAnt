37324a467d8e09956609d3f75679ba666f8e34df
chouchouGG <983897884@qq.com>
2024-12-01 16:36:21 +0800

    完成配置类和启动类

---
+++ ChronAnt-app/src/main/java/cn/uhoc/Application.java
@@ -0,0 +1,19 @@
+package cn.uhoc;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@Configurable
+@EnableScheduling
+@EnableTransactionManagement
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class);
+    }
+
+}
